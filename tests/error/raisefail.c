520183035bae8543aa34324edc92fe943aae214b
Marian Domanski <mmdski@gmail.com>
2022-04-19 20:55:33 -0500

    test failure of err raise

---
+++ tests/error/raisefail.c
@@ -0,0 +1,16 @@
+#include "test.h"
+#include <chl.h>
+
+int
+main ()
+{
+  assert_false (chl_err_stack_is_err ());
+
+  ChlErrorType type = INVALID_ARGUMENT_ERROR;
+  assert_zero (chl_err_raise (type, "message", __FILE__, __LINE__));
+  assert_true (chl_err_stack_is_err ());
+
+  chl_err_raise (type, "message", __FILE__, __LINE__);
+
+  return EXIT_SUCCESS;
+}

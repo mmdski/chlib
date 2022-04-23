d0afe80e395790ede206d7f1589cd5013692a960
Marian Domanski <mmdski@gmail.com>
2022-04-23 17:23:05 -0500

    add chl_pow and chl_abs

--- include/chl/chlmath.h
+++ include/chl/chlmath.h
@@ -12,12 +12,28 @@
 
 #if defined(REAL_IS_FLOAT)
 typedef float real;
+
+#define chl_pow(a, b) powf (a, b)
+
+#define chl_abs(a) fabsf (a)
 #else
 /**
  * @brief Real number type used throughout chlib
  *
  */
 typedef double real;
+
+/**
+ * @brief Package power macro
+ *
+ */
+#define chl_pow(a, b) pow (a, b)
+
+/**
+ * @brief Package absolute value macro
+ *
+ */
+#define chl_abs(a) fabs (a)
 #endif
 
 #endif

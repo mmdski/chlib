be87b5340338e810647800d6167d83aa821104d3
Marian Domanski <mmdski@gmail.com>
2022-05-01 19:39:28 -0500

    add NULL to error macros instead of empty string

--- chlutil/error.h
+++ chlutil/error.h
@@ -1,27 +1,28 @@
 #ifndef __ERR_H__
 #define __ERR_H__
+#include <stdlib.h>
 
 #define RAISE_NULL_ERR_INT                                                     \
   {                                                                            \
-    chl_err_raise (NULL_ARGUMENT_ERROR, "", __FILE__, __LINE__);               \
+    chl_err_raise (NULL_ARGUMENT_ERROR, NULL, __FILE__, __LINE__);             \
     return -1;                                                                 \
   }
 
 #define RAISE_NULL_ERR_NULL                                                    \
   {                                                                            \
-    chl_err_raise (NULL_ARGUMENT_ERROR, "", __FILE__, __LINE__);               \
+    chl_err_raise (NULL_ARGUMENT_ERROR, NULL, __FILE__, __LINE__);             \
     return NULL;                                                               \
   }
 
 #define RAISE_ARG_ERR_INT                                                      \
   {                                                                            \
-    chl_err_raise (INVALID_ARGUMENT_ERROR, "", __FILE__, __LINE__);            \
+    chl_err_raise (INVALID_ARGUMENT_ERROR, NULL, __FILE__, __LINE__);          \
     return -1;                                                                 \
   }
 
 #define RAISE_ARG_ERR_NULL                                                     \
   {                                                                            \
-    chl_err_raise (INVALID_ARGUMENT_ERROR, "", __FILE__, __LINE__);            \
+    chl_err_raise (INVALID_ARGUMENT_ERROR, NULL, __FILE__, __LINE__);          \
     return NULL;                                                               \
   }
 

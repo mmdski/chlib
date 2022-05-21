ce1acb7440164002532df3b11f849c1bcc1c1809
Marian Domanski <mmdski@gmail.com>
2022-05-21 13:23:17 -0500

    add more qs to spec energy plot

--- app/xs_rect/spec_energy.c
+++ app/xs_rect/spec_energy.c
@@ -27,30 +27,64 @@ main (void)
   fputs ("set key left top box font \",20\"\n", gnuplot);
   fputs ("set xlabel \"Specific energy, in m\" font \",15\"\n", gnuplot);
   fputs ("set ylabel \"Depth, in m\" font \",15\"\n", gnuplot);
-  fputs ("plot '-' notitle \"Specific Energy\" with lines\n", gnuplot);
 
   size_t n     = 100;
-  double q     = 10;
-  double y_min = 0.1;
-  double y_max = 5;
-  double dy    = (y_max - y_min) / n;
+  double q1    = 1;
+  double q2    = 5;
+  double q3    = 10;
+  double y_min = 0.001;
+  double y_max = 1;
+  double dy    = (y_max - y_min) / (n - 1);
   double spec_energy;
   double y;
-  double width = 10;
+  double width = 25;
+
+  fprintf (gnuplot, "set xrange [ 0 : %g ] noreverse writeback\n", y_max);
+  fprintf (gnuplot, "set yrange [ 0 : %g ] noreverse writeback\n", y_max);
+
+  fprintf (gnuplot,
+           "plot "
+           "'-' title \"Q = %g cms\" with lines,"   // q1
+           "'-' title \"Q = %g cms\" with lines,"   // q2
+           "'-' title \"Q = %g cms\" with lines\n", // q3
+           q1,
+           q2,
+           q3);
 
   ChlXSRect xs;
   if ((xs = chl_xs_rect_new (width)) == NULL)
     goto fail;
 
+  // plot specific energy for q1
   for (size_t i = 0; i < n; i++)
     {
       y = y_min + i * dy;
-      if (chl_xs_rect_spec_energy (xs, y, q, &spec_energy) < 0)
+      if (chl_xs_rect_spec_energy (xs, y, q1, &spec_energy) < 0)
         goto fail;
       fprintf (gnuplot, "%g %g\n", spec_energy, y);
     }
-
   fputs ("e\n", gnuplot);
+
+  // plot specific energy for q2
+  for (size_t i = 0; i < n; i++)
+    {
+      y = y_min + i * dy;
+      if (chl_xs_rect_spec_energy (xs, y, q2, &spec_energy) < 0)
+        goto fail;
+      fprintf (gnuplot, "%g %g\n", spec_energy, y);
+    }
+  fputs ("e\n", gnuplot);
+
+  // plot specific energy for q3
+  for (size_t i = 0; i < n; i++)
+    {
+      y = y_min + i * dy;
+      if (chl_xs_rect_spec_energy (xs, y, q3, &spec_energy) < 0)
+        goto fail;
+      fprintf (gnuplot, "%g %g\n", spec_energy, y);
+    }
+  fputs ("e\n", gnuplot);
+
   fflush (gnuplot);
   chl_xs_rect_free (xs);
   return EXIT_SUCCESS;
